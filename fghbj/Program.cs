a1a38ffeff7ee2762c6dd1df2fd0d2374b301db8
Tina Li <tina.li@learnet.se>
2020-11-04 14:19:37 +0100

    sfdv

---
+++ fghbj/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Raylib_cs;
+
+namespace fghbj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "GrafikDemo");
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
